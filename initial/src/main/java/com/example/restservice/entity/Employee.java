2f1f19956fb648f1f208a03387c814f7eb71eddb
Joel Salas <joel.salas.093@gmail.com>
2024-02-05 15:26:38 -0500

    Added fields to Employee Class

--- initial/src/main/java/com/example/restservice/entity/Employee.java
+++ initial/src/main/java/com/example/restservice/entity/Employee.java
@@ -1,4 +1,19 @@
 package com.example.restservice.entity;
 
+/*
+DTO for @Employee
+employee_id
+first_name
+last_name
+email
+title
+ */
+
 public class Employee {
+    private int employee_id;
+    private String first_name;
+    private String last_name;
+    private String email;
+    private String title;
+
 }
