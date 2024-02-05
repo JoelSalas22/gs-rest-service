c21a4c205bfe9d3f9d54da6c59bb73adcb16684f
Joel Salas <joel.salas.093@gmail.com>
2024-02-05 16:35:17 -0500

    Added JPA, Lombok

--- initial/src/main/java/com/example/restservice/entity/Employee.java
+++ initial/src/main/java/com/example/restservice/entity/Employee.java
@@ -1,19 +1,24 @@
 package com.example.restservice.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
 /*
-DTO for @Employee
+@Employee Fields
 employee_id
 first_name
 last_name
 email
 title
  */
-
+@Entity
 public class Employee {
+    @Id
+    @Column(name=d")
     private int employee_id;
     private String first_name;
     private String last_name;
     private String email;
     private String title;
-
 }
